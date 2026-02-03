f12551484729ca00d37153760dee41c2e5b0aaae
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-02-03 15:57:49 +0300

    refactor(db): use migrateDrizzle utility in migration plugin

--- test/fixtures/blog/server/plugins/1.migrate.ts
+++ test/fixtures/blog/server/plugins/1.migrate.ts
@@ -1,36 +1,18 @@
-import type { DrizzleDatasources } from '#nuxt-drizzle/virtual/datasources'
-
-const STATEMENT_BREAKPOINT = '--> statement-breakpoint' as const
+import type { DrizzleDatasources, DrizzleDatasourceName } from '#nuxt-drizzle/virtual/datasources'
 
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hookOnce('drizzle:created', async (datasources) => {
     for (const [name, datasource] of Object.entries(datasources)) {
-      const migrations = await useDrizzleMigrations(name as keyof DrizzleDatasources)
-      if (!migrations) {
-        continue
-      }
-
-      for await (const { id, query } of migrations) {
-        const statements = query.split(STATEMENT_BREAKPOINT)
-        try {
-          for (const statement of statements) {
-            await datasource.db.run(statement)
-          }
-        }
-        catch (cause) {
-          throw createError({
-            fatal: true,
-            message: `Migrations for ${JSON.stringify(name)} rolled back`,
-            cause,
-            data: {
-              id,
-              statement: query,
-            },
-          })
-        }
-      }
+      const migrations = await useDrizzleMigrations(name as DrizzleDatasourceName)
+      await migrateDrizzle(datasource, migrations)
     }
 
     nitro.hooks.callHook('drizzle:migrated', datasources)
   })
 })
+
+declare module 'nitropack/types' {
+  interface NitroRuntimeHooks {
+    'drizzle:migrated': (datasources: DrizzleDatasources) => void
+  }
+}
