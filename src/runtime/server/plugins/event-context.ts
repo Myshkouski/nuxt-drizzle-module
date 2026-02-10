975ee13fefc2982a95ce28cd13fa28d56886200a
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-02-11 01:26:33 +0300

    refactor(plugin): move drizzle:created hook call after request hook registration

--- src/runtime/server/plugins/event-context.ts
+++ src/runtime/server/plugins/event-context.ts
@@ -9,12 +9,12 @@ import type { DrizzleDatasources, NamedDrizzleDatasource } from '../utils/types'
 export default defineNitroPlugin(async (nitroApp) => {
   const runtimeConfig = useRuntimeConfig()
   const datasources = await createDatasources(runtimeConfig.drizzle || {})
-  if (Object.keys(datasources).length) {
-    await nitroApp.hooks.callHook('drizzle:created', datasources)
-  }
   nitroApp.hooks.hook('request', event => {
     event.context.drizzle = datasources
   })
+  if (Object.keys(datasources).length) {
+    await nitroApp.hooks.callHook('drizzle:created', datasources)
+  }
 })
 
 async function createDatasources<
