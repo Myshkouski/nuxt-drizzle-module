3f2c315237400905d9fc14aa2117875d2e17f88c
Alexei Myshkouski <am@samplify.org>
2026-02-11 23:06:45 +0300

    refactor(server): remove unused type imports from augments declaration

--- src/runtime/server/augments.server.d.ts
+++ src/runtime/server/augments.server.d.ts
@@ -1,6 +1,4 @@
-import type { HookResult } from 'nuxt/schema'
 import type { DrizzleDatasources, NamedDrizzleDatasource } from './utils/types'
-import type { H3Event } from 'h3'
 
 declare module '#nuxt-drizzle/virtual/datasources' {
   export type { DrizzleDatasources, NamedDrizzleDatasource }
