3f6169a368da4bc1ce6095f39edf8cc4a0097cca
Alexei Myshkouski <am@samplify.org>
2026-02-11 21:39:49 +0300

    refactor(module): move drizzle runtime config type

--- src/runtime/server/augments.server.d.ts
+++ src/runtime/server/augments.server.d.ts
@@ -17,14 +17,4 @@ declare module 'h3' {
   }
 }
 
-declare module '@nuxt/schema' {
-  interface RuntimeConfig {
-    drizzle?: {
-      [TName in DrizzleDatasourceName]?: NamedDrizzleDatasourceFactory<TName>['createDb'] extends (...args: [infer TConfig, ...any]) => any
-        ? TConfig
-        : unknown;
-    } & Record<string, any>
-  }
-}
-
 export {}
