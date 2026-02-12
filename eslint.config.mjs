4a115f4b40a4db2384ab28be2c399ffedfa5577b
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-02-12 10:41:47 +0300

    chore(config): add @typescript-eslint/no-empty-object-type rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ export default createConfigForNuxt({
       'regexp/no-misleading-capturing-group': 'warn',
       'regexp/no-super-linear-backtracking': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
       'vue/multi-word-component-names': 'off',
     },
   })
