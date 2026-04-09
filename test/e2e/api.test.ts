ca6c9073356ba3a266fe20021281b744e880a66f
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-04-09 23:32:55 +0300

    test(e2e): add timeout to test suite

--- test/e2e/api.test.ts
+++ test/e2e/api.test.ts
@@ -1,7 +1,9 @@
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
-describe('drizzle datasources', async () => {
+describe('drizzle datasources', {
+  timeout: 60 * 1000
+}, async () => {
   await setup({
     rootDir: import.meta.dirname,
   })
