72f417b4efa91125988ffd3b9f02075cbd16fe68
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-04-11 22:32:50 +0300

    perf(test-fixtures): execute DB queries in parallel

--- test/fixtures/nitro-api/server/api/v1/content.get.ts
+++ test/fixtures/nitro-api/server/api/v1/content.get.ts
@@ -1,7 +1,9 @@
 export default defineEventHandler(async (event) => {
   const { db, schema } = useDrizzle(event, 'content')
-  const posts = await db.select().from(schema.posts).limit(10)
-  const comments = await db.select().from(schema.comments).limit(10)
+  const [posts, comments] = await Promise.all([
+    db.select().from(schema.posts).limit(10),
+    db.select().from(schema.comments).limit(10),
+  ])
   return {
     posts,
     comments,
